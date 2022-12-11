cd00c3b5801112666941cbe09bc2d5fc1e37f018
Michal Sokolík <xsokolik@fi.muni.cz>
2022-12-11 01:41:24 +0100

    feat: rendering map with marker

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,28 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
 import "./App.css";
 
-function App() {
-	const [count, setCount] = useState(0);
+const position = [51.505, -0.09];
 
+function App() {
 	return (
-		<div className="App">
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src="/vite.svg" className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://reactjs.org" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite - React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
+		<div id="map">
+			<MapContainer
+				center={position}
+				zoom={13}
+				scrollWheelZoom={false}
+				style={{ width: "100vw", height: "100vh" }}
+			>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+				<Marker position={position}>
+					<Popup>
+						A pretty CSS3 popup. <br /> Easily customizable.
+					</Popup>
+				</Marker>
+			</MapContainer>
 		</div>
 	);
 }
