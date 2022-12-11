6509134985ff60b144aea12c00be8273edc8a028
Michal Sokolík <xsokolik@fi.muni.cz>
2022-12-11 02:19:31 +0100

    feat: rendering polygons

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,39 @@
-import { useState } from "react";
-import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+import {} from "react";
+import { MapContainer, TileLayer, Polygon } from "react-leaflet";
+
 import "./App.css";
 
-const position = [51.505, -0.09];
+import Borders from "./data/borders.json";
+
+const position = [49.308877665000068, 20.135855754000119];
 
 function App() {
+	const reverseCoords = (coords: any[]) => {
+		return coords.map((pair) => [pair[1], pair[0]]);
+	};
+
 	return (
 		<div id="map">
 			<MapContainer
 				center={position}
-				zoom={13}
-				scrollWheelZoom={false}
-				style={{ width: "100vw", height: "100vh" }}
+				zoom={5}
+				scrollWheelZoom={true}
+				className="map-container"
 			>
 				<TileLayer
 					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				/>
-				<Marker position={position}>
-					<Popup>
-						A pretty CSS3 popup. <br /> Easily customizable.
-					</Popup>
-				</Marker>
+
+				{/* TODO: rendering multiple polygons */}
+				{Borders.features.map((state) => (
+					<Polygon
+						pathOptions={{ color: "purple" }}
+						positions={state.geometry.coordinates.map(
+							reverseCoords
+						)}
+					/>
+				))}
 			</MapContainer>
 		</div>
 	);
